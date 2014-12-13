11577ccc7235a0f0a821eaa12f91cc59796bfd2a
simone cesano <simone.cesano@gmail.com>
2014-12-13 20:10:36 +0100

    Added paging

--- lib/Vahvuus/Controller/Perforce.pm
+++ lib/Vahvuus/Controller/Perforce.pm
@@ -33,29 +33,32 @@ sub index :Path :Args(0) {
     $c->forward('View::HTML');
 }
 
+use Data::Page;
+
 sub list :Path  {
     my ( $self, $c, @path ) = @_;
 
+    my $page = Data::Page->new();
+    $page->entries_per_page($c->req->params->{c} || 80);
+    $page->current_page($c->req->params->{p} || 1);
+    
     my @out;
     my $m = $c->model('Perforce');
     my $path_re = quotemeta(join '/', '/', @path, '');
     my $path = join '/', '/', @path, '*';
-    my @dirs = $m->dirs(@path)->stdout; $m->close;
-    $c->log->info("Dirs:\n", @dirs);
-    
-    my @files = map { s/#.+//; $_ } $m->run('files', '-e', $path)->stdout; $m->close;
-    $c->log->info("path: ", $path);
-    $c->log->info("Files:\n", @files);
-    
-    @files = $m->run('fstat', '-Ol', '-T', 'depotFile,fileSize,headTime,headModTime', @files)->stdout; $m->close;
 
-    $c->log->info("Files:\n", @files);
-    
+    my @dirs = $m->dirs(@path)->stdout; $m->close;
+    my @files = map { s/#.+//; $_ } $m->run('files', '-e', $path)->stdout; $m->close;
+    $page->total_entries(scalar @files);
+    my @files_page = $page->splice(\@files);
+
+    @files = $m->run('fstat', '-Ol', '-T', 'depotFile,fileSize,headTime,headModTime', @files_page)->stdout; $m->close;
     @files = map { { map { @$_ } @$_ } }                    # transform into hashrefs
     	break { scalar @$_ == 0 }                           # break apart at spaces
     	map { s/^\.\.\. //; [ split ' ', $_, 2 ] } @files;  # clean up
 
 
+    $c->stash->{page} = $page;
     $c->stash->{path} = \@path;
     $c->stash->{folder} = $path[-1];
     $c->stash->{files} = \@files;
